e8d27f55eb194211ab7c70fe8c1fd6e879547be5
lynn <m@taboo.sh>
2025-08-05 18:42:39 +0800

    修复铁砧改名问题，并进行安全性更新

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     java
-    id("io.izzel.taboolib") version "2.0.25" apply false
+    id("io.izzel.taboolib") version "2.0.26" apply false
     id("org.jetbrains.kotlin.jvm") version "1.8.22" apply false
 }
 
@@ -36,9 +36,10 @@ subprojects {
                 Metrics
             )
             disableOnSkippedVersion = false
+            disableWhenPrimitiveLoaderError = true
         }
         version {
-            taboolib = "6.2.3-ac49c9a"
+            taboolib = "6.2.3-ee81cb0"
         }
     }
 
